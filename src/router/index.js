0f29e3a11b9550d037e2ff850e56599da52d4ebf
chenyn <chenyn@ming800.com>
2018-07-19 16:06:37 +0800

    add /404

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,31 @@ import login from '@/views/login';
 
 Vue.use(Router);
 
+// hidden 为 true 代表在导航栏里不显示
+export const constantRouterMap = [{
+    path: '/login',
+    component: () =>
+      import ('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/',
+    redirect: '/login',
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () =>
+      import ('@/views/404'),
+    hidden: true
+  },
+  {
+    path: '*',
+    redirect: '/404',
+    hidden: true
+  }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: login,
-    },
-  ],
+  routes: constantRouterMap
 });
